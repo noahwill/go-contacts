73bccd72b669fabb9c9ec188f00905da9ea50acc
Lawrence <NLawrence@ftportfolios.com>
2019-07-10 15:56:05 -0500

    pull up method

--- controllers/authControllers.go
+++ controllers/authControllers.go
@@ -7,15 +7,21 @@ import (
 	"net/http"
 )
 
-// CreateAccount : decode JSON request into an account
-var CreateAccount = func(w http.ResponseWriter, r *http.Request) {
-	account := &models.Account{}
-	// Decode request body into a struct
+// JSONValidateRequest : decodes the request into JSON and errors out if it is invalid
+func JSONValidateRequest(w http.ResponseWriter, r *http.Request, account *models.Account) {
 	err := json.NewDecoder(r.Body).Decode(account)
 	if err != nil {
 		u.Respond(w, u.Message(false, "Invalid request"))
 		return
 	}
+}
+
+// CreateAccount : decode JSON request into an account
+var CreateAccount = func(w http.ResponseWriter, r *http.Request) {
+	account := &models.Account{}
+
+	// Decode request body into a struct
+	JSONValidateRequest(w, r, account)
 
 	resp := account.Create()
 	u.Respond(w, resp)
@@ -24,12 +30,9 @@ var CreateAccount = func(w http.ResponseWriter, r *http.Request) {
 // Authenticate : uses JSON decoded account to log in
 var Authenticate = func(w http.ResponseWriter, r *http.Request) {
 	account := &models.Account{}
+
 	// Decode request body into a struct
-	err := json.NewDecoder(r.Body).Decode(account)
-	if err != nil {
-		u.Respond(w, u.Message(false, "Invalid request"))
-		return
-	}
+	JSONValidateRequest(w, r, account)
 
 	resp := models.Login(account.Email, account.Password)
 	u.Respond(w, resp)
